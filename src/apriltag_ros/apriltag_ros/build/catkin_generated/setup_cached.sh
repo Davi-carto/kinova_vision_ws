b004eea47f57e90c8c767eb3610b72436c19c72c
carto <davi1390723349outlook.com>
2024-07-03 14:25:44 +0800

    ss

---
+++ src/apriltag_ros/apriltag_ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/zjw/kinova_vision_ws/src/apriltag_ros/apriltag_ros/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/zjw/kinova_vision_ws/src/apriltag_ros/apriltag_ros/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/zjw/kinova_vision_ws/src/apriltag_ros/apriltag_ros/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/zjw/kinova_vision_ws/src/apriltag_ros/apriltag_ros/build'
+export PYTHONPATH="/home/zjw/kinova_vision_ws/src/apriltag_ros/apriltag_ros/build/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROS_PACKAGE_PATH="/home/zjw/kinova_vision_ws/src/apriltag_ros/apriltag_ros:$ROS_PACKAGE_PATH"
---
+++ src/arm_control/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/zjw/kinova_vision_ws/src/arm_control/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
